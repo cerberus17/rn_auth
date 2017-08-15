472431f67cd10f159f45078193c4630c8f163b2e
Adam Kipnis <adamkipnis@yahoo.com>
2017-08-15 11:27:42 -0700

    Added logged in state and logging out action.

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import { View, Text } from 'react-native';
 
 import firebase from 'firebase';
 
-import { Header } from './components/common';
+import { Header, Button, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = {
+    loggedIn: null
+  };
+
   componentDidMount() {
     firebase.initializeApp({
       apiKey: "AIzaSyDXbcDDM4SwIQrHwkAAxLIKpwponcwnels",
@@ -16,13 +20,33 @@ class App extends Component {
       storageBucket: "react-native-authenticat-ccaa3.appspot.com",
       messagingSenderId: "408650070023"
     });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user)
+        this.setState({ loggedIn: true });
+      else
+        this.setState({ loggedIn: false });
+    });
+  }
+
+  renderContent() {
+    if (this.state.loggedIn === true)
+      return (
+          <Button onPress={() => firebase.auth().signOut()}>
+            Log Out
+          </Button>
+      );
+    else if (this.state.loggedIn === false)
+      return <LoginForm/>
+    else
+      return <Spinner size="large" />
   }
 
   render() {
     return (
         <View>
           <Header headerText="Authentication" />
-          <LoginForm/>
+          {this.renderContent()}
         </View>
     )
   }
